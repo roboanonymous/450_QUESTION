ba6f89d43b9fb2e96ddaa401b532407d89269529
roboanonymous <jsamyak591@gmail.com>
2022-05-03 12:46:28 +0530

    first

--- src/factorial.java
+++ src/factorial.java
@@ -1,16 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class factorial {
-	public static long factorial(int n)
+	public static void factorial(int n)
 	{
-		if(n==0) {
-			return 1;
-		}
-		else {
-			return(n* factorial(n-1));
+		ArrayList <Integer> al = new ArrayList <>();
+		int size=0 , carry =0;
+		al.add(0,1);
+		size=1;
+		int val = 2;
+		while (val <= n)
+		{
+			for(int j =size-1; j>=0; j--)
+			{
+				int temp = al.get(j) *val +carry;
+				al.set(j,temp%10);
+				carry = temp/10;
+			}
+			
+			while(carry != 0)
+			{
+				al.add(0,carry%10);
+				carry = carry/10;
+				size++;
+			}
+			val++;
 		}
 		
-		
+		System.out.print(al);
 	}
 
 	public static void main(String[] args) {
@@ -19,7 +35,7 @@ public class factorial {
 		System.out.println("Enter n:");
 		int n = sc.nextInt();
 		
-		System.out.println (factorial(n));
+		factorial(n);
 
 	}
 
